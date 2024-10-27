6d10f399a2a6546ce973a08f58d4b5453ad4e63e
Viktoriia Vyshniak <viktoriia.vyshniak@nure.ua>
2024-10-27 11:26:49 +0200

    Fixed UserImageStore smart-contract

--- src/smart-contracts/user-image-store/contracts/UserImageStore.sol
+++ src/smart-contracts/user-image-store/contracts/UserImageStore.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.27;
 
 contract UserImageStore {
     struct Image {
-        bytes32 userId;
+        string userLogin;
         bytes32 imageHash;
         uint256 imageId;
         bool visiblePublicly;
@@ -12,114 +12,90 @@ contract UserImageStore {
     }
 
     struct User {
-        string password;
+        bytes32 passwordHash;
         string login;
         bool isLoggedIn;
-        bytes32 id;
     }
 
-    mapping(bytes32 => User) private users;
-    bytes32[] private userIds;
+    mapping(string => User) private users;
 
     mapping(uint256 => Image) private images;
     uint256 private nextImageId;
 
-    event UserRegistered(bytes32 indexed id, string login);
-    event UserLoggedIn(bytes32 indexed id);
     event UserLoggedOut(bytes32 indexed id);
     event ImageStored(bytes32 indexed userId, uint256 imageId);
 
     function registerUser(string memory _login, string memory _password) public {
-        bytes32 userId = keccak256(abi.encodePacked(_login, block.timestamp));
-        require(users[userId].id == bytes32(0), "User already exists.");
-
         User memory user;
-        user.id = userId;
         user.login = _login;
-        user.password = _password;
+        user.passwordHash = keccak256(abi.encodePacked(_password));
         user.isLoggedIn = false;
-        users[userId] = user;
-
-        userIds.push(userId);
-        emit UserRegistered(userId, _login);
+        users[_login] = user;
     }
 
-    function loginUser(bytes32 _userId, string memory _password) public {
-        User storage user = users[_userId];
-        require(user.id != bytes32(0), "User does not exist.");
-        require(keccak256(abi.encodePacked(user.password)) == keccak256(abi.encodePacked(_password)), "Incorrect password.");
-        require(!user.isLoggedIn, "User already logged in.");
+    function loginUser(string memory userLogin, string memory password) public {
+        User storage user = users[userLogin];
+        require(!user.isLoggedIn, "User is already logged in.");
+        require(keccak256(abi.encodePacked(userLogin)) == keccak256(abi.encodePacked(user.login)), "Incorrect login.");
+        require(keccak256(abi.encodePacked(password)) == user.passwordHash, "Incorrect password.");
 
         user.isLoggedIn = true;
-        emit UserLoggedIn(_userId);
     }
 
-    function logoutUser(bytes32 _userId) public {
-        User storage user = users[_userId];
-        require(user.id != bytes32(0), "User does not exist.");
+    function logoutUser(string memory userLogin) public {
+        User storage user = users[userLogin];
         require(user.isLoggedIn, "User is not logged in.");
 
         user.isLoggedIn = false;
-        emit UserLoggedOut(_userId);
     }
 
     function storeImage(
-        bytes32 _userId,
+        string memory userLogin,
         bytes32 _imageHash,
         bool _visiblePublicly,
         string memory _imageName,
         bool _canBeExchanged
     ) public {
-        User storage user = users[_userId];
-        require(user.id != bytes32(0), "User does not exist.");
+        User storage user = users[userLogin];
         require(user.isLoggedIn, "User must be logged in to store images.");
 
         uint256 imageId = nextImageId++; 
         images[imageId] = Image({
-            userId: _userId,
+            userLogin: userLogin,
             imageHash: _imageHash,
             imageId: imageId,
             visiblePublicly: _visiblePublicly,
             imageName: _imageName,
             canBeExchanged: _canBeExchanged
         });
-
-        emit ImageStored(_userId, imageId);
     }
 
-    function getImagesByUserId(bytes32 _userId) public view returns (bytes32[] memory, uint256[] memory, bool[] memory, string[] memory, bool[] memory) {
-        require(users[_userId].id != bytes32(0), "User does not exist.");
+    function getImagesByUserLogin(string memory userLogin) public view returns (Image[] memory) {
+        User storage user = users[userLogin];
+        require(user.isLoggedIn, "User must be logged in to store images.");
 
         uint256 userImageCount = 0;
 
         for (uint256 i = 0; i < nextImageId; i++) {
-            if (images[i].userId == _userId) {
+            if (keccak256(abi.encodePacked(images[i].userLogin)) == keccak256(abi.encodePacked(userLogin))) {
                 userImageCount++;
             }
         }
 
-        bytes32[] memory imageHashes = new bytes32[](userImageCount);
-        uint256[] memory imageIds = new uint256[](userImageCount);
-        bool[] memory visiblePublicly = new bool[](userImageCount);
-        string[] memory imageNames = new string[](userImageCount);
-        bool[] memory canBeExchanged = new bool[](userImageCount);
-
+        Image[] memory userImages = new Image[](userImageCount);
         uint256 index = 0;
+
         for (uint256 i = 0; i < nextImageId; i++) {
-            if (images[i].userId == _userId) {
-                imageHashes[index] = images[i].imageHash;
-                imageIds[index] = images[i].imageId;
-                visiblePublicly[index] = images[i].visiblePublicly;
-                imageNames[index] = images[i].imageName;
-                canBeExchanged[index] = images[i].canBeExchanged;
+            if (keccak256(abi.encodePacked(images[i].userLogin)) == keccak256(abi.encodePacked(userLogin))) {
+                userImages[index] = images[i];
                 index++;
             }
         }
 
-        return (imageHashes, imageIds, visiblePublicly, imageNames, canBeExchanged);
+        return userImages;
     }
 
-    function getImages() public view returns (bytes32[] memory, uint256[] memory, string[] memory) {
+    function getImages() public view returns (Image[] memory) {
         uint256 totalPublicImages = 0;
 
         for (uint256 i = 0; i < nextImageId; i++) {
@@ -128,20 +104,16 @@ contract UserImageStore {
             }
         }
 
-        bytes32[] memory imageHashes = new bytes32[](totalPublicImages);
-        uint256[] memory imageIds = new uint256[](totalPublicImages);
-        string[] memory imageNames = new string[](totalPublicImages);
+        Image[] memory publicImages = new Image[](totalPublicImages);
         uint256 index = 0;
 
         for (uint256 i = 0; i < nextImageId; i++) {
             if (images[i].visiblePublicly) {
-                imageHashes[index] = images[i].imageHash;
-                imageIds[index] = images[i].imageId;
-                imageNames[index] = images[i].imageName;
+                publicImages[index] = images[i];
                 index++;
             }
         }
 
-        return (imageHashes, imageIds, imageNames);
+        return publicImages;
     }
 }
--- src/smart-contracts/user-image-store/scripts/deploy.js
+++ src/smart-contracts/user-image-store/scripts/deploy.js
@@ -1,6 +1,6 @@
 const Web3Lib = require('web3');
 const { abi, bytecode } = require('./compile');
-const { expect } = require("chai")
+const { expect } = require("chai");
 
 const web3 = new Web3Lib.Web3('http://127.0.0.1:8545');
 
@@ -14,75 +14,61 @@ const deploy = async () => {
         .send({ from: accounts[0], gas: '3000000' });
 };
 
-const testFunctionality = async () => {
-    describe('UserImageStore Contract', function () {
-        let userId1, userId2;
-    
-        it('should deploy the contract', async () => {
-            expect(userImageStore.options.address).to.exist;
-        });
-    
-        it('should register users', async () => {
-            await userImageStore.methods.registerUser("testUser1", "testPassword1").send({ from: accounts[0] });
-            await userImageStore.methods.registerUser("testUser2", "testPassword2").send({ from: accounts[1] });
-    
-            userId1 = web3.utils.keccak256(web3.eth.abi.encodeParameters(['string'], ["testUser1"]));
-            userId2 = web3.utils.keccak256(web3.eth.abi.encodeParameters(['string'], ["testUser2"]));
-    
-            expect(userId1).to.be.a('string');
-            expect(userId2).to.be.a('string');
-        });
-    
-        it('should allow users to login', async () => {
-            await userImageStore.methods.loginUser(userId1, "testPassword1").send({ from: accounts[0] });
-            await userImageStore.methods.loginUser(userId2, "testPassword2").send({ from: accounts[1] });
-        });
-    
-        it('should store images for users', async () => {
-            await userImageStore.methods.storeImage(userId1, web3.utils.keccak256("image1Hash"), 1, true, "Image1", true).send({ from: accounts[0] });
-            await userImageStore.methods.storeImage(userId2, web3.utils.keccak256("image2Hash"), 2, false, "Image2", false).send({ from: accounts[1] });
-        });
-    
-        it('should retrieve images by userId', async () => {
-            const imagesUser1 = await userImageStore.methods.getImagesByUserId(userId1).call();
-            const imagesUser2 = await userImageStore.methods.getImagesByUserId(userId2).call();
-    
-            expect(imagesUser1.imageHashes).to.be.an('array').that.is.not.empty;
-            expect(imagesUser2.imageHashes).to.be.an('array').that.is.not.empty;
-            expect(imagesUser1.imageNames[0]).to.equal("Image1");
-            expect(imagesUser2.imageNames[0]).to.equal("Image2");
-        });
-    
-        it('should retrieve publicly visible images from all users', async () => {
-            const publicImages = await userImageStore.methods.getImages().call();
-    
-            expect(publicImages.imageHashes).to.be.an('array').that.is.not.empty;
-            expect(publicImages.imageNames[0]).to.equal("Image1");
-        });
-    
-        it('should log out a user', async () => {
-            await userImageStore.methods.logoutUser(userId1).send({ from: accounts[0] });
-            const userLoggedOut = await userImageStore.methods.isLoggedIn(userId1).call();
-            expect(userLoggedOut).to.be.false;
-        });
-    
-        it('should fail to store an image after logout', async () => {
-            try {
-                await userImageStore.methods.storeImage(userId1, web3.utils.keccak256("image3Hash"), 3, true, "Image3", false).send({ from: accounts[0] });
-                expect.fail('Expected error not received');
-            } catch (error) {
-                expect(error.message).to.include('revert');
-            }
-        });
-    });    
-};
+describe('UserImageStore Contract', function () {
+    let userId1, userId2;
 
-const runTests = async () => {
-    await deploy();
-    await testFunctionality();
-    console.log('All tests completed!');
-};
+    before(async () => {
+        await deploy();
+    });
 
-runTests().catch(err => {
-    console.error(err);
+    it('should deploy the contract', async () => {
+        expect(userImageStore.options.address).to.exist;
+    });
+
+    it('should register users', async () => {
+        await userImageStore.methods.registerUser("testUser1", "testPassword1").send({ from: accounts[0] });
+        await userImageStore.methods.registerUser("testUser2", "testPassword2").send({ from: accounts[1] });
+
+        userId1 = web3.utils.keccak256(web3.eth.abi.encodeParameters(['string'], ["testUser1"]));
+        userId2 = web3.utils.keccak256(web3.eth.abi.encodeParameters(['string'], ["testUser2"]));
+
+        expect(userId1).to.be.a('string');
+        expect(userId2).to.be.a('string');
+    });
+
+    it('should allow users to login', async () => {
+        await userImageStore.methods.loginUser("testUser1", "testPassword1").send({ from: accounts[0] });
+        await userImageStore.methods.loginUser("testUser2", "testPassword2").send({ from: accounts[1] });
+    });
+
+    it('should store images for users', async () => {
+        await userImageStore.methods.storeImage("testUser1", web3.utils.keccak256("image1Hash"), true, "Image1", true).send({ from: accounts[0] });
+        await userImageStore.methods.storeImage("testUser2", web3.utils.keccak256("image2Hash"), false, "Image2", false).send({ from: accounts[1] });
+    });
+
+    it('should retrieve images by userLogin', async () => {
+        const imagesUser1 = await userImageStore.methods.getImagesByUserLogin("testUser1").call();
+        const imagesUser2 = await userImageStore.methods.getImagesByUserLogin("testUser2").call();
+
+        expect(imagesUser1).to.be.an('array').that.is.not.empty;
+        expect(imagesUser2).to.be.an('array').that.is.not.empty;
+        expect(imagesUser1[0].imageName).to.equal("Image1");
+        expect(imagesUser2[0].imageName).to.equal("Image2");
+    });
+
+    it('should retrieve publicly visible images from all users', async () => {
+        const publicImages = await userImageStore.methods.getImages().call();
+
+        expect(publicImages).to.be.an('array').that.is.not.empty;
+        expect(publicImages[0].imageName).to.equal("Image1");
+    });
+
+    it('should log out a user and fail getImagesByUserLogin after logout', async () => {
+        await userImageStore.methods.logoutUser("testUser1").send({ from: accounts[0] });
+        try {
+            await userImageStore.methods.getImagesByUserLogin("testUser1").call();
+        } catch (error) {
+            console.log("getImagesByUserLogin failed after logout as expected:", error.cause.errorArgs);
+        }
+    });
 });
